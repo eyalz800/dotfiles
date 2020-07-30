37f62c2d566b46c9cd24792e1bb7eb661700abb7
eyalz800 <eyalz800@gmail.com>
2020-07-30 13:59:21 +0300

    Added install xclip.

--- install/install_tmux.sh
+++ install/install_tmux.sh
@@ -14,6 +14,7 @@ sudo apt-get install -y autotools-dev
 sudo apt-get install -y automake
 sudo apt-get install -y libncurses5-dev libncursesw5-dev
 sudo apt-get install -y libevent-dev
+sudo apt-get install -y xclip
 
 if ! [ -d ~/tmp ]; then mkdir ~/tmp; fi
 cd ~/tmp || die "cannot chdir to ~/tmp"
